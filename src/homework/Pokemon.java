05871b962b95022c6e494d1cc7fd1d48fd456eed
JiSeong Kim <12180543@inha.edu>
2023-02-12 01:56:01 +0900

    230212: Pokemon 클래스 구현

--- src/homework/Pokemon.java
+++ src/homework/Pokemon.java
@@ -1,6 +1,41 @@
 package homework;
+
+import java.util.ArrayList;
+
 public class Pokemon {
-    public static void main(String[] args) {
-        System.out.println("Hello Pokemon!");
+    private String owner;
+    private ArrayList<String> skills;
+
+    public Pokemon(String owner, ArrayList<String> skills) {
+        this.owner = owner;
+        this.skills = skills;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(ArrayList<String> skills) {
+        this.skills = skills;
+    }
+
+    public void info() {
+        System.out.println(this.owner + "의 포켓몬이 사용 가능한 스킬");
+        for (int i = 0; i < skills.size(); i++) {
+            System.out.print((i + 1) + " : " + skills.get(i));
+            System.out.println();
+        }
+    }
+
+    public void attack(int i) {
+        System.out.print(skills.get(i) + "공격 시전!");
     }
 }
