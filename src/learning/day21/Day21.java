7ba7609af356086c75ab64715b1d52dd9f8a9a39
JiSeong Kim <12180543@inha.edu>
2023-02-13 17:09:58 +0900

    230213: 람다식 객체 정렬, echoclass 생성

---
+++ src/learning/day21/Day21.java
@@ -0,0 +1,46 @@
+package learning.day21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+class Circle implements Comparable{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public String toString() {
+        return String.format("원의 반지름은 %s 입니다.", radius);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Circle c = (Circle) o;
+        return (int) (getArea() - c.getArea());
+    }
+}
+
+
+public class Day21 {
+    public static void main(String[] args){
+        Circle[] circles = { new Circle(5.0), new Circle(2.5), new Circle(10.0)};
+        Arrays.sort(circles);
+        for (Circle c : circles) {
+            System.out.println(c);
+        }
+    }
+}
