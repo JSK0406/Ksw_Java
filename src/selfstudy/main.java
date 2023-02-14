90effcc5238a3475e8aedf98f67b7f6dcf531b3f
JiSeong Kim <12180543@inha.edu>
2023-02-15 02:04:14 +0900

    230215: stream04

--- src/selfstudy/main.java
+++ src/selfstudy/main.java
@@ -6,23 +6,39 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+class Fruit {
+    private String name;
+    private int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
 
 public class main {
     private static int result;
 
     public static void main(String[] args) {
-//        IntStream st = IntStream.rangeClosed(1, 10);
-////        st.forEach(System.out::println);
-//        st.forEach(n -> result = result + n);
-//        System.out.println(result);
+        List<Fruit> fruitList = Arrays.asList(
+                new Fruit("watermelon", 16000),
+                new Fruit("watermelon", 19000),
+                new Fruit("strawberry", 10000),
+                new Fruit("kiwi", 13000),
+                new Fruit("banana", 16000)
+        );
 
-//        String[] fruits = {"키위", "딸기", "바나나", "수박"};
-//        Stream<String> fruitsStream = Arrays.stream(fruits);
-//        fruitsStream.forEach(f -> System.out.println(f));
-//
-//        double[] nums = {3.14, 2.71, 99.99, -9.123};
-//        DoubleStream doubleStream = Arrays.stream(nums);
-//        doubleStream.forEach(System.out::println);
+//        Stream<Fruit> st = fruitList.stream();
+//        fruitList.stream().sorted((f1, f2) -> f1.getPrice() - f2.getPrice()).forEach(f -> System.out.println(f.getName() + " "));
+//        fruitList.stream().sorted(Comparator.reverseOrder()).forEach(f -> System.out.println(f.getName() + " "));
 
     }
 }
