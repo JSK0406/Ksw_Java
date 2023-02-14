ac5c2bcc4820b347ee031cbbceadcaef1b0479cd
JiSeong Kim <12180543@inha.edu>
2023-02-15 01:47:53 +0900

    230215: stream03

--- src/selfstudy/main.java
+++ src/selfstudy/main.java
@@ -1,49 +1,28 @@
 package selfstudy;
 
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class Fruit {
-    private String name;
-    private int price;
 
-    public Fruit(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-}
 public class main {
+    private static int result;
+
     public static void main(String[] args) {
-        List<Fruit> fruitList = Arrays.asList(
-                new Fruit("watermelon", 16000),
-                new Fruit("watermelon", 19000),
-                new Fruit("strawberry", 10000),
-                new Fruit("kiwi", 13000),
-                new Fruit("banana", 16000)
-        );
-        System.out.println(
-                fruitList
-                        .stream()
-                       // .mapToInt(f -> f.getPrice())
-                        .mapToInt(Fruit::getPrice)
-                        .average()
-                        .getAsDouble()
-        );
+//        IntStream st = IntStream.rangeClosed(1, 10);
+////        st.forEach(System.out::println);
+//        st.forEach(n -> result = result + n);
+//        System.out.println(result);
 
-
-//        fruitList.stream().forEach(f -> {
-//            String name = f.getName();
-//            int price = f.getPrice();
-//            System.out.println(name + " : " + price);
-//        });
+//        String[] fruits = {"키위", "딸기", "바나나", "수박"};
+//        Stream<String> fruitsStream = Arrays.stream(fruits);
+//        fruitsStream.forEach(f -> System.out.println(f));
+//
+//        double[] nums = {3.14, 2.71, 99.99, -9.123};
+//        DoubleStream doubleStream = Arrays.stream(nums);
+//        doubleStream.forEach(System.out::println);
 
     }
 }
