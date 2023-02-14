7ee2afffae45ab75f51d8f44641318440b16033a
JiSeong Kim <12180543@inha.edu>
2023-02-15 01:06:22 +0900

    230215: stream01

--- src/selfstudy/main.java
+++ src/selfstudy/main.java
@@ -1,58 +1,21 @@
 package selfstudy;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class main {
     public static void main(String[] args) {
-//        Map<String, Integer> soldiers = Map.of("최일구", 3, "박민석", 2, "차병호", 1);
-//        System.out.println(soldiers);
-//        System.out.println(soldiers.size());
-//        System.out.println(soldiers.get("최일구"));
-//
-//        Map<String, Integer> soldiers2 = new HashMap<>(soldiers);
-//        soldiers2.put("심우진", 4);
-//        System.out.println(soldiers2.get("심우진"));
+        List<String> cities = Arrays.asList(
+                "서울", "부산", "광주", "대구", "부산", "서울", "대전", "서울", "경기", "대구");
+        Iterator<String> it = cities.iterator();
+        while(it.hasNext())
+            System.out.print(it.next() + " ");
+        System.out.println();
 
-//        long s, e;
-//        ArrayList<Integer> a1 = new ArrayList<>();
-//        LinkedList<Integer> l1 = new LinkedList<>();
-//
-//        s = System.nanoTime();
-//        for(int i = 0; i < 100000; i++)
-//         a1.add(0, i);
-//        e = System.nanoTime();
-//        System.out.println(e-s);
-//
-//        s = System.nanoTime();
-//        for(int i = 0; i < 100000; i++)
-//            l1.addFirst(i);
-//        e = System.nanoTime();
-//        System.out.println(e-s);
-
-
-//        Stack<Integer> s1 = new Stack<>();
-//
-//        s1.push(1);
-//        s1.push(2);
-//        s1.push(3);
-//        System.out.println(s1.peek());
-//        System.out.println(s1.peek());
-//        System.out.println(s1.pop());
-//        System.out.println(s1.pop());
-
-
-//        List<String> soldier = List.of("차병호", "성윤모");
-//        System.out.println(soldier.contains("차병호"));
-//        System.out.println(soldier.indexOf("성윤모"));
-//
-//        List<String> s1 = new ArrayList<>(soldier);
-//        s1.add("박민석");
-//        s1.add("김상훈");
-//        s1.forEach((s) -> System.out.print(s + " "));
-//        System.out.println();
-//        s1.removeIf((s) -> s.contains("박"));
-//        s1.forEach((s) -> System.out.print(s + " "));
-
+        Stream<String> st = cities.stream();
+//        st.forEach((s) -> System.out.print(s + " "));
+//        st.filter((s) -> s.contains("대")).forEach((s) -> System.out.print(s + " "));
+        st.distinct().forEach(s -> System.out.print(s + " "));
 
 
     }
