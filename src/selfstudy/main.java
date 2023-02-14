b866cf55e14e65f6875ed535cd99aff8a8def1f9
JiSeong Kim <12180543@inha.edu>
2023-02-15 01:28:30 +0900

    230215: stream02

--- src/selfstudy/main.java
+++ src/selfstudy/main.java
@@ -3,20 +3,47 @@ package selfstudy;
 import java.util.*;
 import java.util.stream.Stream;
 
+class Fruit {
+    private String name;
+    private int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
 public class main {
     public static void main(String[] args) {
-        List<String> cities = Arrays.asList(
-                "서울", "부산", "광주", "대구", "부산", "서울", "대전", "서울", "경기", "대구");
-        Iterator<String> it = cities.iterator();
-        while(it.hasNext())
-            System.out.print(it.next() + " ");
-        System.out.println();
+        List<Fruit> fruitList = Arrays.asList(
+                new Fruit("watermelon", 16000),
+                new Fruit("watermelon", 19000),
+                new Fruit("strawberry", 10000),
+                new Fruit("kiwi", 13000),
+                new Fruit("banana", 16000)
+        );
+        System.out.println(
+                fruitList
+                        .stream()
+                       // .mapToInt(f -> f.getPrice())
+                        .mapToInt(Fruit::getPrice)
+                        .average()
+                        .getAsDouble()
+        );
 
-        Stream<String> st = cities.stream();
-//        st.forEach((s) -> System.out.print(s + " "));
-//        st.filter((s) -> s.contains("대")).forEach((s) -> System.out.print(s + " "));
-        st.distinct().forEach(s -> System.out.print(s + " "));
 
+//        fruitList.stream().forEach(f -> {
+//            String name = f.getName();
+//            int price = f.getPrice();
+//            System.out.println(name + " : " + price);
+//        });
 
     }
 }
