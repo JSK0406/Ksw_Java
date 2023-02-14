ce460f8740276dd4bce6461d0860c3a0eb1b6867
JiSeong Kim <12180543@inha.edu>
2023-02-14 18:07:47 +0900

    230214: 스레드 과제 02

--- src/homework/day22/Day22.java
+++ src/homework/day22/Day22.java
@@ -1,23 +1,17 @@
 package homework.day22;
 
-class Worker extends Thread {
-//    static boolean isStop = false;
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("작업 스레드 : " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-//        isStop = true;
-    }
-}
-
 public class Day22 {
     public static void main(String[] args) throws InterruptedException {
-        Thread worker = new Worker();
+        Thread worker = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("작업 스레드 : " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         worker.start();
         int alphabet = (int) 'a';
         while (true) {
