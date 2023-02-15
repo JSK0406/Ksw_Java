f530f6015d9579a3d767fe59ecb91ad68ac1d21b
JiSeong Kim <12180543@inha.edu>
2023-02-15 16:33:33 +0900

    230215: homework01

--- src/learning/day23/Day23.java
+++ src/learning/day23/Day23.java
@@ -1,8 +1,16 @@
 package learning.day23;
 
-public class Day23 {
+class One {
+    private One() {
+    }
+    public static One produceOneInstance() {
+        return new One();
+    }
+}
 
+public class Day23 {
     public static void main(String[] args) {
+        One o = One.produceOneInstance();
 
     }
 }
