cabf034ba2bc2bdec3a850ad4313530c1e79f8b9
JiSeong Kim <12180543@inha.edu>
2023-02-16 14:10:20 +0900

    230216: SimpleEchoServer 계산기

--- src/learning/day21/SimpleEchoServer.java
+++ src/learning/day21/SimpleEchoServer.java
@@ -6,54 +6,66 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-public class SimpleEchoServer {
+public class SimpleEchoServer implements Runnable {
+    // 다중 접속 에코 서버
+    private static Socket clientSocket;
+    public SimpleEchoServer(Socket clientSocket)
+    {
+        this.clientSocket = clientSocket;
+    }
     public static void main(String[] args) {
-        System.out.println("에코 서버 시작됨");
-        try (ServerSocket serverSocket = new ServerSocket(6000)) {
-            System.out.println("클라이언트 접속 대기 중.....");
-            Socket clientSocket = serverSocket.accept();  // 접속 대기
-            System.out.println("클라이언트 접속됨.");
+        ExecutorService eService = Executors.newFixedThreadPool(5);  // 5 threads
+        System.out.println("다중 접속 에코 서버");
 
-            try (
-                    BufferedReader br = new BufferedReader(
-                            new InputStreamReader(clientSocket.getInputStream()));
-                    PrintWriter pw =
-                            new PrintWriter(clientSocket.getOutputStream(), true)) {
-                Supplier<String> socketIn = () -> {
-                    try {
-                        return br.readLine();
-                    } catch (IOException ex) {
-                        return null;
-                    }
-                };
-                Stream s = Stream.generate(socketIn);
-//                s.map(text -> {
-//                    System.out.println("클라이언트로부터 받은 메세지 : " + text);
-//                    pw.println(text);
-//                    return text;
-//                }).allMatch(t -> t != null);
-
-//                s.peek(text -> {
-//                    System.out.println("클라이언트로부터 받은 메세지 : " + text);
-//                    pw.println(text);
-//                }).allMatch(Object::nonNull);
-
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    System.out.println("클라이언트로 부터 받은 메세지 : " + line);
-//                    pw.println(line);  // 클라이언트로 송신
-//                }
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try (ServerSocket serverSocket = new ServerSocket(20000)) {
+            while (true) {
+                System.out.println("클라이언트 접속 대기 중.....");
+                clientSocket = serverSocket.accept();
+                SimpleEchoServer tes = new SimpleEchoServer(clientSocket);
+                eService.submit(tes);
             }
+        } catch (IOException ex) {
+            System.out.println("입출력 예외 발생!");
         }
-        catch (IOException ex) {
-            System.out.println("접속 실패!");
+        System.out.println("다중 접속 에코 서버 종료");
+        eService.shutdown();
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread() + " 스레드 접속");
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                System.out.println(clientSocket.getRemoteSocketAddress().toString() + " " + Thread.currentThread() +" 클라이언트가 보낸 메세지 : " + inputLine);
+                StringTokenizer st = new StringTokenizer(inputLine,"+-*/", true);
+                int result = Integer.parseInt(st.nextToken());
+                while (st.hasMoreTokens()) {
+                    switch (st.nextToken()) {
+                        case "+" -> result += Integer.parseInt(st.nextToken());
+                        case "-" -> result -= Integer.parseInt(st.nextToken());
+                        case "*" -> result *= Integer.parseInt(st.nextToken());
+                        case "/" -> result /= Integer.parseInt(st.nextToken());
+                    }
+                }
+                out.println(inputLine + " = " + Integer.toString(result));
+            }
+            System.out.println(Thread.currentThread() +" 클라이언트가 종료됨"); }
+        catch (IOException ex)
+        {
+            System.out.println("입출력 예외 발생!");
         }
     }
 }
